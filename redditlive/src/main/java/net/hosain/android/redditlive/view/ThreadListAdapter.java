6ed16d6755eaa95e7ce2b1fd3701be4bf770fe40
Hosain <contact@hosain.net>
2015-12-14 14:29:06 +0000

    Split RecyclerView adapter to a separate class

---
+++ redditlive/src/main/java/net/hosain/android/redditlive/view/ThreadListAdapter.java
@@ -0,0 +1,73 @@
+package net.hosain.android.redditlive.view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import net.hosain.android.redditlive.R;
+import net.hosain.android.redditlive.dummy.DummyContent;
+
+import java.util.List;
+
+public class ThreadListAdapter
+        extends RecyclerView.Adapter<ThreadListAdapter.ViewHolder> {
+
+    private final List<DummyContent.DummyItem> mValues;
+
+    public ThreadListAdapter(List<DummyContent.DummyItem> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.thread_list_content, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Intent intent = new Intent(context, ThreadDetailActivity.class);
+                intent.putExtra(ThreadDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public DummyContent.DummyItem mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.id);
+            mContentView = (TextView) view.findViewById(R.id.content);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
